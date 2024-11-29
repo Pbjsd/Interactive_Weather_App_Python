92e08bc4d560bd5fc8d33597dce126035ac9547c
Sandeep Dhaliwal <104098660+Pbjsd@users.noreply.github.com>
2024-11-29 03:05:46 +0000

    Added user input and validation

---
+++ main.py
@@ -0,0 +1,7 @@
+city_name = input("Enter a city: ")
+city_name = city_name.strip()
+
+if city_name: 
+  print(f"The temperature in {city_name} is 28°C")
+else: 
+  print("Please try again with a city")
